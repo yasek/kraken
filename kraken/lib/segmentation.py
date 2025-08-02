9c7535457160ab7c39f2fb6dca811fb5b451873e
Serge <yasek@users.noreply.github.com>
2025-08-02 18:14:26 +0100

    Update segmentation.py

--- kraken/lib/segmentation.py
+++ kraken/lib/segmentation.py
@@ -34,6 +34,7 @@ from shapely.ops import nearest_points, unary_union
 from shapely.validation import explain_validity
 from skimage import draw, filters
 from skimage.filters import sobel
+from cucim.skimage.filters import sato
 from skimage.graph import MCP_Connect
 from skimage.measure import (approximate_polygon, label, regionprops,
                              subdivide_polygon)
@@ -317,7 +318,7 @@ def vectorize_lines(im: np.ndarray, threshold: float = 0.17, min_length=5,
     st_map = im[0]
     end_map = im[1]
     bl_map = im[2]
-    bl_map = filters.sato(bl_map, black_ridges=False, mode='constant')
+    bl_map = sato(bl_map, black_ridges=False, mode='constant')
     bin_bl_map = bl_map > threshold
     # skeletonize
     line_skel = skeletonize(bin_bl_map)
